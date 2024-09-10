13787c733e158644af986d3ffd12f42ca94cd212
igor <igor3108almeida@gmail.com>
2024-09-09 21:54:51 -0300

    adicionando aula05

---
+++ aulas/aula05/src/pages/FormLogin.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Botao from "../components/Botao";
+import InputEmail from "../components/InputEmail";
+import InputSenha from "../components/InputSenha";
+
+function FormLogin(props) {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const aoClicar = (e) => {
+        e.preventDefault();
+       props.navegaPara("/home")
+    }
+  return (
+  <>
+  <InputEmail valor={email} mudaValor={setEmail} />
+  <InputSenha valor={senha} mudaValor={setSenha} />
+  <Botao texto = "Entrar" aoClicar={aoClicar} />
+
+   </>
+  );
+}
+
+export default FormLogin;
